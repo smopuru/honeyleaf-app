fb6e8b9d770a01efe342a3172c581bb5731f6dcb
Saurabh Sachan <saurabhsachan@swiggy.in>
2015-12-23 18:00:17 +0530

    initial app commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suriApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the suriApp
+ */
+angular.module('suriApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suriApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the suriApp
+ */
+angular.module('suriApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
